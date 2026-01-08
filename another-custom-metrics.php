18d6f0b09691467c8d720b2326d808dbf0e2a39f
chomusuke-png <owokenji@gmail.com>
2026-01-08 17:00:03 -0300

    ajustes menores

--- another-custom-metrics.php
+++ another-custom-metrics.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Another Custom Metrics
  * Description: Sistema de gestión de widgets de estadísticas mediante CPT y Shortcodes.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Zumito
  * Text Domain: another-custom-metrics
  */
